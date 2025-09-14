856b706e1bfd7b1bd055139a49805d73a669ebd5
Jonander <jonander.eg@gmail.com>
2025-09-14 13:39:43 +0200

    feat: router func for workers

---
+++ internal/router/router.go
@@ -0,0 +1,60 @@
+package router
+
+import (
+	"context"
+	"hash/fnv"
+
+	"github.com/jonandereg/streamforge/internal/events"
+)
+
+// StartRouter creates numWorkers output channels and starts a goroutine that
+// routes each TickMsg to a deterministic worker index based on Tick.Symbol.
+
+func StartRouter(ctx context.Context, in <-chan events.TickMsg, numWorkers, queueCap int) []chan events.TickMsg {
+	if numWorkers <= 0 {
+		numWorkers = 1
+	}
+	if queueCap <= 0 {
+		queueCap = 0
+	}
+
+	outs := make([]chan events.TickMsg, numWorkers)
+	for i := range outs {
+		outs[i] = make(chan events.TickMsg, queueCap)
+	}
+
+	go func() {
+		defer func() {
+			for _, ch := range outs {
+				close(ch)
+			}
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case msg, ok := <-in:
+				if !ok {
+					return
+				}
+				idx := workerIndex(msg.Tick.Symbol, numWorkers)
+				select {
+				case outs[idx] <- msg:
+				case <-ctx.Done():
+					return
+				}
+
+			}
+		}
+	}()
+
+	return outs
+}
+
+// workerIndex maps a symbol to a stable worker index in [0, numWorkers).
+func workerIndex(symbol string, numWorkers int) int {
+	h := fnv.New32a()
+	_, _ = h.Write([]byte(symbol))
+	return int(h.Sum32() % uint32(numWorkers))
+
+}
